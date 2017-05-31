ca9240ad9e0cdc5d8edc491f48bf4eab69222e54
MichielStock <michiel.stock@telenet.be>
2017-05-31 21:59:19 +0200

    save autoencoder

--- src/models/labels_auto_encoder.py
+++ src/models/labels_auto_encoder.py
@@ -1,10 +1,14 @@
 import keras
 import argparse
 import h5py
-import json
+#import json_file
+#import json
 import sys
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout
+from keras.models import load_model
+#from keras.models import model_from_json
+
 
 def build_label_autoencoder(name, noise_input=0.2, size_layers=[20, 10],
         dropout_layers=[0.25, 0.25], n_code=8, epochs=50, batch_size=100):
@@ -21,7 +25,7 @@ def build_label_autoencoder(name, noise_input=0.2, size_layers=[20, 10],
 
 
     # inputs
-    inputs = Input((n_labels,))
+    inputs = Input((n_labels,), name='input')
     layer_output = Dropout(rate=noise_input)(inputs)
 
     # encoding
@@ -29,13 +33,19 @@ def build_label_autoencoder(name, noise_input=0.2, size_layers=[20, 10],
         layer_output = Dense(units=n_hidden, activation='relu')(layer_output)
         layer_output = Dropout(rate=dropout_rate)(layer_output)
 
-    encoding = Dense(units=n_code, activation='sigmoid')(layer_output)
+    encoding = Dense(units=n_code, activation='sigmoid', name='encoding')(layer_output)
     layer_output = encoding
 
     # decoding
+    first_layer = True
     for n_hidden, dropout_rate in zip(reversed(size_layers),
                                                     reversed(dropout_layers)):
-        layer_output = Dense(units=n_hidden, activation='relu')(layer_output)
+        if first_layer:
+            layer_output = Dense(units=n_hidden, activation='relu',
+            name='decoding')(layer_output)
+            first_layer = False
+        else:
+            layer_output = Dense(units=n_hidden, activation='relu')(layer_output)
         layer_output = Dropout(rate=dropout_rate)(layer_output)
 
     outputs = Dense(units=n_labels, activation='sigmoid')(layer_output)
@@ -47,13 +57,21 @@ def build_label_autoencoder(name, noise_input=0.2, size_layers=[20, 10],
     model.fit(x=y_train, y=y_train, batch_size=batch_size, epochs=epochs,
                                                     shuffle=True)
     model.save('../models/{}_weights.h5'.format(name))
-    with open('../models/{}_architecture.json'.format(name), 'w') as json_file:
-        json_file.write(model.to_json())
+
+    #with open('../models/{}_architecture.json'.format(name), 'w') as json_file:
+    #    json_file.write(model.to_json())
 
 def load_autoencoder(name):
     """
     Loads a pretrained autoencoder and returns the encoder and decoder
     """
+    model = load_model('../models/{}_weights.h5'.format(name))
+    encoder = Model(inputs=model.input,
+                            outputs=model.get_layer('encoding').output)
+    decoder = Model(inputs=model.get_layer('decoding').input,
+                            outputs=model.output)
+    return encoder, decoder
+    #model = model_from_json('../models/{}_architecture.json'.format(name))
 
 def main():
     parser = argparse.ArgumentParser(description="")
@@ -66,7 +84,6 @@ def main():
     parser.add_argument('-b', '--batch_size', type=int, default=100, help="batch size")
 
     args = parser.parse_args()
-    print(args)
 
     build_label_autoencoder(**vars(args))
 
