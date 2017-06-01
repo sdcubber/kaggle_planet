3897aa91014525267361bb135057c6bf13bd9cd3
sdcubber <stijn.decubber@ugent.be>
2017-06-01 17:02:21 +0200

    example class for simple autoencoder

--- src/models/models.py
+++ src/models/models.py
@@ -48,6 +48,51 @@ def GFM_power_classifier(n_labels, features, predictions, unique_combinations):
         E_F.append(E_f)
     return(np.array(optimal_predictions), E_F)
 
+class simple_autoencoder(object):
+    def __init__(self, input_dim=17, encoding_dim=8):
+
+        self.nbits = encoding_dim
+        self.input_seq = Input(shape=(input_dim,))
+
+        # "encoded" is the encoded representation of the input
+        self.encoded = Dense(self.nbits, activation='sigmoid')(self.input_seq)
+        # "decoded" is the lossy reconstruction of the input
+        decoded = Dense(input_dim, activation='sigmoid')(self.encoded)
+
+        self.autoencoder = Model(self.input_seq, decoded)
+        self.autoencoder.compile(loss='binary_crossentropy',
+                           optimizer='adadelta', metrics=['accuracy'])
+
+        self.trained=False
+
+    def fit(self, input_train, input_val, n_epochs=100, verbose=0):
+        self.history = self.autoencoder.fit(input_train, input_train,
+                            epochs=n_epochs,
+                            batch_size=200,
+                            validation_data=(input_val, input_val), verbose=verbose)
+
+        self.trained=True
+
+    def encode(self, input_seq):
+        assert self.trained, 'Autoencoder has not been trained yet.'
+        encoder = Model(self.input_seq, self.encoded)
+        return(encoder.predict(input_seq, verbose=1))
+
+
+    def decode(self, input_code):
+        assert self.trained, 'Autoencoder has not been trained yet.'
+        encoded_input = Input(shape=(self.nbits,))
+        decoder_layer = self.autoencoder.layers[-1]
+
+        decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+        return(decoder.predict(input_code, verbose=1))
+
+    def __str__(self):
+        return """Simple autoencoder from the Keras blog. See https://blog.keras.io/building-autoencoders-in-keras.html"""
+
+
+
 def DataAugmenter():
 	"""Return ImageDataGenerator with specified settings"""
 	generator = ImageDataGenerator(featurewise_center=False,
@@ -87,7 +132,7 @@ class FFNN(object):
 		self.input = input_layer
 
 	def __str__(self):
-		"""Simple feed-forward neural network with two layers."""
+		return """Simple feed-forward neural network with two layers."""
 
 class SimpleCNN(object):
 	def __init__(self, size, output_size):
@@ -157,8 +202,7 @@ class UNet(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""Downsampling part of a UNet model with a classifier stacked on top.
-		Best performing model so far."""
+		return """Downsampling part of a UNet model with a classifier stacked on top."""
 
 class UNet_dstl(object):
 	def __init__(self, size, output_size):
@@ -202,7 +246,7 @@ class UNet_dstl(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""Downsampling part of a UNet model with a classifier stacked on top.
+		return """Downsampling part of a UNet model with a classifier stacked on top.
 		Some adaptations according to https://deepsense.io/deep-learning-for-satellite-imagery-via-image-segmentation/:
 			* same number of channels across scales
 			* model is fully convolutional
@@ -272,10 +316,6 @@ class SimpleNet64_2_plus_par(object):
 		conv_4_nir = Dropout(0.5)(conv_4_nir)
 		conv_4_nir_out = GlobalAveragePooling2D()(conv_4_nir)
 
-
-
-
-
 		# Concatenate all the outputs
 		conv_concatenated = k.layers.concatenate([conv_1_rgb_out, conv_2_rgb_out, conv_3_rgb_out, conv_4_rgb_out, conv_1_nir_out, conv_2_nir_out, conv_3_nir_out, conv_4_nir_out])
 
@@ -288,7 +328,7 @@ class SimpleNet64_2_plus_par(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""SimpleNet2"""
+        return """SimpleNet2"""
 
 
 class SimpleNet64_2_plus(object):
@@ -340,7 +380,7 @@ class SimpleNet64_2_plus(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""SimpleNet2"""
+        return """SimpleNet2"""
 
 class SimpleNet64_2(object):
 	def __init__(self, size, output_size):
@@ -391,7 +431,7 @@ class SimpleNet64_2(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""SimpleNet2"""
+		return """SimpleNet2"""
 
 class SimpleNet_multitask(object):
 	def __init__(self, size, output_size_multiclass, output_size_multilabel):
@@ -444,6 +484,6 @@ class SimpleNet_multitask(object):
 		self.input = image_input
 
 	def __str__(self):
-		"""SimpleNet2 with two different outputs to incorporate prior knowledge about the labels.
+        return """SimpleNet2 with two different outputs to incorporate prior knowledge about the labels.
 		Weather labels are treated as multiclass classification (as should be the case)
 		All other labels are treated as multilabel classification"""
