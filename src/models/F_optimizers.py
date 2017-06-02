78702ec17a36b2adbe4f808045207677c2309839
sdcubber <stijn.decubber@ugent.be>
2017-06-02 11:01:27 +0200

    Made F-optimizers more verbose for easier debugging

--- src/models/F_optimizers.py
+++ src/models/F_optimizers.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 26 16:01:57 2017
-F optimizers
+F optimizers by threshold adjustment
 @author: stijndc
 """
 import os
@@ -13,22 +13,26 @@ from sklearn.metrics import fbeta_score
 import random
 
 
-
 def find_thresholds(y_train, p_train, y_valid, p_valid):
-    best_anokas = optimise_f2_thresholds(y_train, p_train)
-
     fixed_threshold = 0.2
-    p_valid_binary = p_valid > fixed_threshold
-    score_valid = fbeta_score(y_valid, p_valid_binary, beta=2, average='samples')
-    print('Score on the validation set with fixed threshold {}: {}'.format(fixed_threshold, score_valid))
 
-    p_valid_binary_best = p_valid > best_anokas
-    score_valid_best = fbeta_score(y_valid, p_valid_binary_best, beta=2, average='samples')
-    print('Score on the validation set with best threshold: {}'.format(score_valid_best))
-    print('Optimized thresholds: {}'.format(best_anokas))
-    return(best_anokas)
+    score_train = fbeta_score(y_train, p_train > fixed_threshold, beta=2, average='samples')
+    score_valid = fbeta_score(y_valid, p_valid > fixed_threshold, beta=2, average='samples')
 
+    print('Score on the training data with fixed threshold {}: {}'.format(fixed_threshold, score_train))
+    print('Score on the validation data with fixed threshold {}: {}'.format(fixed_threshold, score_valid))
 
+    # Look for optimal threshold on the training data
+    optimized_threshold = optimise_f2_thresholds(y_train, p_train)
+
+    score_train_best = fbeta_score(y_train, p_train > optimized_threshold, beta=2, average='samples')
+    score_valid_best = fbeta_score(y_valid, p_valid > optimized_threshold, beta=2, average='samples')
+
+    print('Score on the training data with best threshold: {}'.format(score_train_best))
+    print('Score on the validation data with best threshold: {}'.format(score_valid_best))
+    print('Optimized thresholds: {}'.format(optimized_threshold))
+
+    return(optimized_threshold)
 
 def optimise_f2_thresholds(y, p, verbose=False, resolution=100):
     """Optimize individual thresholds one by one. Code from anokas.
@@ -64,24 +68,7 @@ def optimise_f2_thresholds(y, p, verbose=False, resolution=100):
     return x
 
 
-def truncate_probabilities(probabilities):
-    """If an image is cloudy with high confidence, set all other probabilities to zero."""
-    probabilities[probabilities[:, 6] > 0.8,:] = [0]*6 + [0.8] + [0]*10
-    return(probabilities)
-
-# Find best f2 score threshold
-
-
-def truncate_weathertags(probabilities):
-    """Set all but one probabilities of weather tags to zero. Retain the weather tag with the largest posterior probability."""
-    for r in range(probabilities.shape[0]):
-        i = np.argmax(probabilities[r,[5,6,10,11]])
-        probabilities[r,[5,6,10,11]] = 0
-        probabilities[r, i] = 1
-
-    return(probabilities)
-
-def find_f2score_threshold(p_valid, y_valid):
+def find_f2score_threshold(y_valid, p_valid):
     p_valid, y_valid = np.array(p_valid), np.array(y_valid)
 
     best = 0
@@ -96,6 +83,18 @@ def find_f2score_threshold(p_valid, y_valid):
     print('Best score: ', round(best_score, 5), ' @ threshold =', best)
 
     return best
+
+
+
+def truncate_weathertags(probabilities):
+    """Set all but one probabilities of weather tags to zero. Retain the weather tag with the largest posterior probability."""
+    for r in range(probabilities.shape[0]):
+        i = np.argmax(probabilities[r,[5,6,10,11]])
+        probabilities[r,[5,6,10,11]] = 0
+        probabilities[r, i] = 1
+
+    return(probabilities)
+
 
 
 def find_f2score_threshold_2D(p_0, p_1, y_valid):
@@ -114,28 +113,3 @@ def find_f2score_threshold_2D(p_0, p_1, y_valid):
 
     print('Best score: ', round(best_score, 5), '@ thresholds = ', (best_0, best_1))
     return((best_0,best_1))
-
-
-# Find best threshold for each label individually with sim annealing?
-
-
-#def return_score(threshold_set, p_set, true_set):
-    #"""Return fbeta score for a set of predicted probabilities with a set of thresholds.
-    #One threshold per label"""
-    #return(fbeta_score(true_set, p_set > threshold_set, beta=2, average='samples'))
-
-#f_score = lambda t_set, p_set, l_set: 1 - fbeta_score(l_set, p_set > t_set, beta=2, average='samples')
-
-#def find_f2score_threshold_simannealing(p_set, l_set, best=0.2):
-#    """Find best set of individual thresholds on a set of data and true labels."""
-    #x0 = [random.random() for i in range(17)]
-#    x0 = [best + random.random()/10. for i in range(17)]
-#    print(x0)
-#    minimizer_kwargs = {'args': (p_set, l_set, )}
-#    best_set = scipy.optimize.basinhopping(func=f_score,
-#                                           x0=x0,
-#                                           minimizer_kwargs=minimizer_kwargs,
-#                                           niter=50,
-#                                           disp=True, T=2.0, stepsize=0.1)
-#
-#    return(best_set)
